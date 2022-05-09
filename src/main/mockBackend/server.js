cf31924a6470c9b5ccd0a9fdada71f32940a8006
c-atfish <jonlar@student.kristiania.no>
2022-05-09 17:58:35 +0200

    Added basic functionality of fetching your groups from the backend, will display all groups.
    Also added functionality to click on the group and display the name of the group members sendt from the server

---
+++ src/main/mockBackend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import * as path from "path";
+
+
+const app = express();
+
+const groupNames = [{groupname: "PRO201-G8", members: ["Lil J", "Per åge", "Bernt Kåre"]}, {groupname: "TK2100", members: ["Svein Torleif", "Bjertulf sveinson", "Sigurd Knutsen"]}, {groupname: "Smidig prosjekt", members: ["Per Erling"]}, {groupname: "Avansert Java", members: ["Dag Marie", "Berntulf Jensen", "Kåre Uleifson"]}, {groupname: "PG6301 API Design", members: ["Odgunn Bordsann", "Fredvar Komodesen"]}]
+
+
+app.get("/api/v1/groups", (req, res) => {
+    res.json(groupNames)
+})
+
+app.use(express.static("../web/dist"))
+
+app.use((req, res, next) => {
+    if(req.method === "GET" && !req.path.startsWith("/api")){
+        res.sendFile(path.resolve("../client/dist/index.html"))
+    }else{
+        next();
+    }
+})
+
+app.listen(3000, ()=> {
+    console.log("Server started at http://localhost:3000")
+})
--- src/main/web/index.jsx
+++ src/main/web/index.jsx
@@ -1,24 +1,108 @@
-import React, {useEffect, useState} from "react";
+import React from "react"
 import ReactDOM from "react-dom";
+import {BrowserRouter, Link, Route, Routes, useLocation, useNavigate} from "react-router-dom";
+import {useLoader} from "./useLoader.jsx";
+import {fetchJSON} from "./fetchJSON.jsx";
 
-function Application() {
-    const [data,setData] = useState();
-    useEffect(async () => {
-        const res = await fetch("/foo/api/something");
-        if (res.ok) {
-            setData(await res.text());
-        }
-    })
 
-    if (!data) {
+function GroupLinks() {
+
+    // Should get the data for the group names you are a part of
+
+    const navigate = useNavigate();
+
+    const {data, error, loading} = useLoader(() => fetchJSON("/api/v1/groups"))
+
+    if(loading){
         return <div>Loading...</div>
     }
 
+    if(error){
+        return <div>
+            <h2>Error</h2>
+            <h3>{error}</h3>
+        </div>
+    }
 
     return <div>
-        <h1>Hello World</h1>
-        <pre>{data.toString()}</pre>
-    </div>
+        <div><Link to={"/createGroup"}>Opprett gruppe</Link></div>
+        <div><Link to={"/searchGroup"}>Søk etter gruppe</Link></div>
+        <div>
+            <h2>Mine grupper</h2>
+            {data.map((group) => (
+                <div>
+                    <div key={group.groupname}>
+                        <button onClick={() =>  navigate("/group/specific", {state: {group}})}>{group.groupname}</button>
+                    </div>
+                </div>
+            ) )}
+            <h2>atCampus</h2>
+            <h3>Get unstuck</h3>
+            <div>
+                <ul>
+                    <li>Regler</li>
+                    <li>Bruksvilkår</li>
+                    <li>Salgsvilkår</li>
+                    <li>Personvernsærklæring</li>
+                    <li>Om atcampus</li>
+                    <li>Gi tilbakemelding</li>
+                    <li>Ledige stillinger</li>
+                </ul>
+            </div>
+        </div>
+    </div>;
+}
+
+function SearchGroup() {
+    return <div>Search</div>;
+}
+
+function CreateGroup() {
+    return <div>Create</div>;
+}
+
+function ShowMyGroup() {
+    const location = useLocation()
+
+    const navigate = useNavigate()
+    const group = location.state.group
+
+    return <div>
+        <h2>{group.groupname}</h2>
+        <h2>Chat</h2>
+        <h2>Møtekalender</h2>
+        <h2>Notater</h2>
+        <button onClick={() => navigate("/group/members", {state: {group}})}>Medlemmer</button>
+    </div>;
+}
+
+function GroupMembers() {
+    const location = useLocation()
+
+    const group = location.state.group
+
+    return <div>
+        <h2>Medlemmer</h2>
+        <ul>
+            {group.members.map((member => (
+                <div key={member}>
+                    <li>{member}</li>
+                </div>
+            )))}
+        </ul>
+    </div>;
+}
+
+function Application() {
+    return  <BrowserRouter>
+        <Routes>
+            <Route path={"/"} element={<GroupLinks/>}/>
+            <Route path={"/searchGroup"} element={<SearchGroup/>}/>
+            <Route path={"/createGroup"} element={<CreateGroup/>}/>
+            <Route path={"/group/specific"} element={<ShowMyGroup/>}/>
+            <Route path={"/group/members"} element={<GroupMembers/>}/>
+        </Routes>
+    </BrowserRouter>
 }
 
 ReactDOM.render(<Application />, document.getElementById("app"));
