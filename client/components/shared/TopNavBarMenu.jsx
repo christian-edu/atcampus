9ac30e35717ffccb5685f18c4ea984946b6f7ba0
Emilio Garcia <emilio.garcia@mail.com>
2022-05-11 18:00:24 +0200

    topnav menu modal wip

--- client/components/shared/TopNavBarMenu.jsx
+++ client/components/shared/TopNavBarMenu.jsx
@@ -1,19 +1,26 @@
 import { Link } from 'react-router-dom';
+import { HomeIcon } from '@heroicons/react/solid';
+import { ChevronRightIcon } from '@heroicons/react/solid';
 
 const TopNavBarMenu = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to='/'>Forsiden</Link>
+    <div className='grid grid-cols-1 bg-purple-1 w-screen text-white px-4'>
+      <ul className='divide-y divide-purple-4'>
+        
+        <li className='flex flex-row justify-between py-4'>
+          <Link to='/' className='flex flex-row'>
+            <HomeIcon className='h-6 w-6 text-white mr-4'/>
+            Forsiden
+          </Link>
+          <ChevronRightIcon className='h-6 w-6 text-white mr-4'/>
         </li>
-        <li>Still spørsmål</li>
-        <li>Topplisten</li>
-        <li>Flashcard</li>
-        <li>Dokumentering</li>
-        <li>Min Profil</li>
-        <li>Mine Spørsmål</li>
-        <li>Mine svar</li>
+        <li className='py-4'>Still spørsmål</li>
+        <li className='py-4'>Topplisten</li>
+        <li className='py-4'>Flashcard</li>
+        <li className='py-4'>Dokumentering</li>
+        <li className='py-4'>Min Profil</li>
+        <li className='py-4'>Mine Spørsmål</li>
+        <li className='py-4'>Mine svar</li>
       </ul>
     </div>
   );
