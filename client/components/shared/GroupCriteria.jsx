105f456efc70ab7edd7ccdcb91b628978f904bae
Sigurd Kvamme <sigurdkvamme@live.no>
2022-05-11 22:53:31 +0200

    added style to group criteria form

--- client/components/shared/GroupCriteria.jsx
+++ client/components/shared/GroupCriteria.jsx
@@ -5,49 +5,59 @@ const GroupCriteria = () => {
 
   return (
     <form onSubmit={submitHandler}>
-      <h2>Søk etter gruppekriterier</h2>
-      <h4>Velg kriterier for søket</h4>
-      <div>
+      <div className='grid gap-4'>
         <div>
-          <select name='emne'>
+          <select
+            name='emne'
+            className='w-full p-2 border border-purple-3 rounded-standard bg-dark-6 mt-2'
+          >
             <option value='Programmering'>Programmering</option>
             <option value='Frontend'>Frontend</option>
             <option value='InteraktivtDesign'>InteraktivtDesign</option>
           </select>
         </div>
         <div>
-          <select name='størrelse'>
+          <select
+            name='størrelse'
+            className='w-full p-2 border border-purple-3 rounded-standard bg-dark-6 mt-2'
+          >
             <option value='liten'>Liten (1-4stk)</option>
             <option value='Medium'>Liten (5-7stk)</option>
             <option value='Stor'>Stor (8+)</option>
           </select>
         </div>
+        <div>
+          <select
+            name='karaktermål'
+            className='w-full p-2 border border-purple-3 rounded-standard bg-dark-6 mt-2'
+          >
+            <option value='A'>A</option>
+            <option value='B'>B</option>
+            <option value='C'>C</option>
+            <option value='D'>D</option>
+            <option value='E'>E</option>
+            <option value='F'>F</option>
+          </select>
+        </div>
+        <div>
+          <select
+            name='arbeidsfrekvens'
+            className='w-full p-2 border border-purple-3 rounded-standard bg-dark-6 mt-2'
+          >
+            <option value='Månedlig'>Månedlig</option>
+            <option value='Ukentlig'>Ukentlig</option>
+          </select>
+        </div>
+        <div>
+          <input type='radio' name={'metode'} id={'fysisk'} />
+          <label htmlFor='fysisk'>Fysisk</label>
+          <input type='radio' name={'metode'} id={'digitalt'} />
+          <label htmlFor='digitalt'>Digitalt</label>
+          <input type='radio' name={'metode'} id={'begge'} />
+          <label htmlFor='begge'>Begge</label>
+        </div>
+        <button>Vis resultater</button>
       </div>
-      <div>
-        <select name='karaktermål'>
-          <option value='A'>A</option>
-          <option value='B'>B</option>
-          <option value='C'>C</option>
-          <option value='D'>D</option>
-          <option value='E'>E</option>
-          <option value='F'>F</option>
-        </select>
-      </div>
-      <div>
-        <select name='arbeidsfrekvens'>
-          <option value='Månedlig'>Månedlig</option>
-          <option value='Ukentlig'>Ukentlig</option>
-        </select>
-      </div>
-      <div>
-        <input type='radio' name={'metode'} id={'fysisk'} />
-        <label htmlFor='fysisk'>Fysisk</label>
-        <input type='radio' name={'metode'} id={'digitalt'} />
-        <label htmlFor='digitalt'>Digitalt</label>
-        <input type='radio' name={'metode'} id={'begge'} />
-        <label htmlFor='begge'>Begge</label>
-      </div>
-      <button>Vis resultater</button>
     </form>
   );
 };
