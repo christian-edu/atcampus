530e8d88cd91c653c2f25d99887974150a72f6a4
Sigurd Kvamme <sigurdkvamme@live.no>
2022-05-11 16:34:32 +0200

    updated modal component

---
+++ client/components/shared/Modal.jsx
@@ -0,0 +1,18 @@
+import ReactDom from 'react-dom';
+
+const Modal = (props) => {
+  const content = (
+    <>
+      <div
+        className='fixed overflow-hidden z-10 w-screen h-screen top-0 left-0 bg-[rgba(0,0,0,0.8)]'
+        onClick={props.onClick}
+      ></div>
+      <div className='fixed h-auto top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%] z-20'>
+        {props.children}
+      </div>
+    </>
+  );
+
+  return <>{ReactDom.createPortal(content, document.querySelector('body'))}</>;
+};
+export default Modal;
--- client/components/shared/UserCard.jsx
+++ client/components/shared/UserCard.jsx
@@ -1,23 +1,33 @@
 import { DotsHorizontalIcon, PlusIcon } from '@heroicons/react/solid';
+import { useState } from 'react';
+import Modal from './Modal';
 
 const UserCard = (props) => {
   const classes = `h-6 w-6 text-purple-1`;
+  const [modalIsVisible, setModalIsVisible] = useState(false);
 
-  const clickHandler = () => {
-    props.onClick();
-  };
+  const toggleModal = () => setModalIsVisible((state) => !state);
+
+  const content = (
+    <div className='bg-white p-16 rounded-standard'>
+      <h2 className='text-lg'>{props.user}</h2>
+    </div>
+  );
 
   return (
-    <li className='bg-white flex items-center p-6 rounded-standard border border-purple-4'>
-      <h2 className='font-bold'>{props.user}</h2>
-      <button onClick={clickHandler} className='ml-auto'>
-        {props.edit ? (
-          <DotsHorizontalIcon className={classes} />
-        ) : (
-          <PlusIcon className={classes} />
-        )}
-      </button>
-    </li>
+    <>
+      {modalIsVisible && <Modal onClick={toggleModal}>{content}</Modal>}
+      <li className='bg-white flex items-center p-6 rounded-standard border border-purple-4'>
+        <h2 className='font-bold'>{props.user}</h2>
+        <button onClick={toggleModal} className='ml-auto'>
+          {props.edit ? (
+            <DotsHorizontalIcon className={classes} />
+          ) : (
+            <PlusIcon className={classes} />
+          )}
+        </button>
+      </li>
+    </>
   );
 };
 export default UserCard;
