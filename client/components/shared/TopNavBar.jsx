7c7be3ae52277370559cc5ff2c04da5885373719
Emilio Garcia <emilio.garcia@mail.com>
2022-05-11 16:47:58 +0200

    topnav kål

--- client/components/shared/TopNavBar.jsx
+++ client/components/shared/TopNavBar.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import TopNavBarMenu from './TopNavBarMenu';
 import { useNavigate } from 'react-router-dom';
+import { MenuIcon } from '@heroicons/react/solid';
+import { ArrowLeftIcon } from '@heroicons/react/solid';
 
 const TopNavBar = () => {
   const [showNavBar, setShowNavBar] = useState(false);
@@ -14,10 +16,20 @@ const TopNavBar = () => {
   const showNavBarHandler = () => setShowNavBar((state) => !state);
 
   return (
-    <div>
+    <div className='flex flex-row py-4 px-4 bg-purple-5 items-center'>
+
       {showNavBar ? <TopNavBarMenu /> : <h2></h2>}
-      <button onClick={backButtonHandler}>&larr;</button>
-      <button onClick={showNavBarHandler}> Menu</button>
+        
+        <button onClick={backButtonHandler} className='flex items-center px-0 py-2 outline outline-2 outline-purple-1 rounded text-white '>
+          <ArrowLeftIcon className="h-6 w-6 text-purple-1"/>
+          &larr;
+        </button>
+
+        <button onClick={showNavBarHandler} className="flex flex-col items-center">
+          <MenuIcon className="h-7 w-7 text-purple-1"/>
+          Menu
+        </button>
+
     </div>
   );
 };
