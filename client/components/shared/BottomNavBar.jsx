8e995c5e3bef37e21fefb2aa80abb7abe612befd
Emilio Garcia <emilio.garcia@mail.com>
2022-05-11 15:29:20 +0200

    Update BottomNavBar.jsx

--- client/components/shared/BottomNavBar.jsx
+++ client/components/shared/BottomNavBar.jsx
@@ -18,21 +18,22 @@ const BottomNavBar = () => {
       {showProfile ? (
         <ProfileMenu showProfileFn={showProfileFn} />
       ) : (
-        <div>
-          <div style={{ display: 'inline-block', marginRight: 20 }}>
+        <div className='flex justify-between px-6 py-4 fixed inset-x-0 bottom-0 bg-dark-6 text-sm'>
+          <div className='flex-col items-center'>
             <Link to={'/questions'}>
+              <p>Test</p>
               <h4>Spørsmål</h4>
             </Link>
           </div>
-          <div style={{ display: 'inline-block', marginRight: 20 }}>
+          <div>
             <h4>Flashcards</h4>
           </div>
-          <div style={{ display: 'inline-block', marginRight: 20 }}>
+          <div>
             <Link to={'/'}>
               <h4>Grupper</h4>
             </Link>
           </div>
-          <div style={{ display: 'inline-block' }}>
+          <div>
             <button onClick={showProfileFn}>Profile</button>
           </div>
         </div>
