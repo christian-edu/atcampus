b044f8fcddbf1d8c3af7c75d65d87c5a7ec68989
Emilio Garcia <emilio.garcia@mail.com>
2022-05-12 12:50:06 +0200

    div request styling 🎨

--- client/components/shared/SearchRequest.jsx
+++ client/components/shared/SearchRequest.jsx
@@ -1,14 +1,25 @@
+import {
+  XIcon,
+  CheckIcon
+} from "@heroicons/react/solid";
+
 const SearchRequest = (props) => {
 
   // Popup som vises når man søker etter gruppe på navn og trykker på den
   return (
-    <div className='bg-white p-8 rounded text-center'>
+    <div className='flex flex-col items-center bg-white p-8 rounded text-center shadow-xl'>
       <h2 className='font-bold text-xl mb-3 w-full border-b-2 pb-4 border-purple-1'>
         {props.group?.groupname || props.user}
       </h2>
       <p className='mb-4'>{props.group ? "Send forespørsel om medlemskap" : "Ønsker du å invitere til gruppen?"}</p>
-      <button onClick={props.onClick}>X</button>
-      <button>V</button>
+      <div className="flex flex-row gap-6">
+        <button className="bg-dark-1 text-white p-2 rounded" onClick={props.onClick}>
+          <XIcon className="h-6 w-6"/>
+        </button>
+        <button className="bg-gradient-left text-white p-2 rounded">
+          <CheckIcon className="h-6 w-6"/>
+        </button>
+      </div>
     </div>
   );
 };
