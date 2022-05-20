61dc6b2a15f19237e71da944a197d92c90d5a6e7
Sigurd Kvamme <sigurdkvamme@live.no>
2022-05-20 10:49:55 +0200

    created breadcrumbs component

---
+++ client/components/shared/Breadcrumbs.jsx
@@ -0,0 +1,27 @@
+import { ChevronRightIcon } from '@heroicons/react/solid';
+import { Link } from 'react-router-dom';
+import useBreadcrumbs from 'use-react-router-breadcrumbs';
+
+const Breadcrumbs = () => {
+  const breadcrumbs = useBreadcrumbs();
+
+  console.log(breadcrumbs);
+
+  return (
+    <ul className='flex'>
+      {breadcrumbs.map(({ key, breadcrumb }, i) => {
+        if (i === breadcrumbs.length - 1)
+          return <li key={key}>{breadcrumb}</li>;
+        return (
+          <li key={key} className='text-purple-1'>
+            <Link to={key} className='flex items-center'>
+              {breadcrumb} <ChevronRightIcon className='h-4' />
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Breadcrumbs;
