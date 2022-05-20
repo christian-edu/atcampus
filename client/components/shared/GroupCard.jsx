acd718c592dce7eb3e61f3b34e371c71208c56f1
Christian Gregersen <christian@gregersen.eu>
2022-05-20 07:22:24 +0200

    La til import av React i GroupCard.jsx og Image.jsx

--- client/components/shared/GroupCard.jsx
+++ client/components/shared/GroupCard.jsx
@@ -1,17 +1,17 @@
-import { PlusIcon } from '@heroicons/react/solid';
-import { useState } from 'react';
-import Modal from './Modal';
-import SearchRequest from './SearchRequest';
-import MatchRequest from './MatchRequest';
-import { UserGroupIcon, LockClosedIcon } from '@heroicons/react/solid';
-import { LockOpenIcon } from '@heroicons/react/outline';
-import Image from './Image';
-
+import { PlusIcon } from "@heroicons/react/solid";
+import { useState } from "react";
+import Modal from "./Modal";
+import SearchRequest from "./SearchRequest";
+import MatchRequest from "./MatchRequest";
+import { UserGroupIcon, LockClosedIcon } from "@heroicons/react/solid";
+import { LockOpenIcon } from "@heroicons/react/outline";
+import Image from "./Image";
+import React from "react";
 const GroupCard = (props) => {
   // Group Card component
 
   // classes for private icon
-  const privateIconClasses = 'h-4 absolute top-4 right-4';
+  const privateIconClasses = "h-4 absolute top-4 right-4";
 
   // State for modals
   const [modalIsVisible, setModalIsVisible] = useState(false);
@@ -49,21 +49,21 @@ const GroupCard = (props) => {
       )}
 
       <div
-        className='bg-white flex items-center gap-2 relative p-6 rounded-standard border border-purple-4 cursor-pointer hover:drop-shadow-xl duration-300'
+        className="bg-white flex items-center gap-2 relative p-6 rounded-standard border border-purple-4 cursor-pointer hover:drop-shadow-xl duration-300"
         onClick={clickHandler}
       >
-        <Image group className='h-14' />
+        <Image group className="h-14" />
         <div>
-          <h3 className='text-dark-1 text-lg font-bold'>{name}</h3>
-          <div className='flex flex-row items-center text-dark-3 mt-2'>
-            <UserGroupIcon className='h-5 w-5 mr-2' />
+          <h3 className="text-dark-1 text-lg font-bold">{name}</h3>
+          <div className="flex flex-row items-center text-dark-3 mt-2">
+            <UserGroupIcon className="h-5 w-5 mr-2" />
 
-            <p className='text-md'>Medlemmer: {groupMember.length}</p>
+            <p className="text-md">Medlemmer: {groupMember.length}</p>
           </div>
         </div>
         {/* Render plus icon if search or match is true */}
         {(props.search || props.match) && (
-          <PlusIcon className='h-6 text-purple-1 ml-auto' />
+          <PlusIcon className="h-6 text-purple-1 ml-auto" />
         )}
 
         {!(props.search || props.match) && privateIcon}
--- client/components/shared/Image.jsx
+++ client/components/shared/Image.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Image = ({ group, className }) => {
   // Dummy component
 
@@ -6,14 +8,14 @@ const Image = ({ group, className }) => {
   console.log(randomNumber());
 
   const path = `https://res.cloudinary.com/dplcm73vn/image/upload/v1652214917/atcampus/${
-    group ? 'group' : 'user'
+    group ? "group" : "user"
   }/${randomNumber()}.jpg`;
 
   return (
     <img
       src={path}
       className={`rounded-full border-2 border-purple-1 ${className}`}
-      alt='group image'
+      alt="group image"
     />
   );
 };
