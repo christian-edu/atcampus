23ad6982be0e3532732df2ad6bac20ddde7cc292
Emilio Garcia <emilio.garcia@mail.com>
2022-05-23 13:35:17 +0200

    User settings popup wip

--- client/components/shared/UserCard.jsx
+++ client/components/shared/UserCard.jsx
@@ -4,6 +4,11 @@ import Image from './Image';
 import MatchRequest from './MatchRequest';
 import Modal from './Modal';
 import SearchRequest from './SearchRequest';
+import Button from './Button';
+import {
+  KeyIcon,
+  TrashIcon
+} from '@heroicons/react/solid';
 SearchRequest;
 
 const UserCard = (props) => {
@@ -32,14 +37,15 @@ const UserCard = (props) => {
 
   // Dummy content for edit modal window
   const content = (
-    <div className='bg-white p-16 rounded-standard'>
-      {/*<h2 className='text-lg'>{user.username}</h2>*/}
-        <div>
-            <button onClick={makeAdmin}>Gjør til admin</button>
-        </div>
-        <div>
-            <button onClick={removeMember}>Fjern medlem</button>
-        </div>
+    <div className='flex flex-col items-center bg-white text-dark-1 p-8 rounded text-center shadow-xl'>
+      <Image group className='h-20 mb-4 -mt-16' />
+      <h2 className='font-bold text-lg w-full border-b-2 pb-4 border-purple-1 mb-5'>
+        {user.username}
+      </h2>
+      <div className='flex flex-col gap-3'>
+          <Button onClick={makeAdmin} className="flex items-center gap-3"><KeyIcon className='h-5 w-5'/>Gjør til admin</Button>
+          <Button onClick={removeMember} className={'flex items-center gap-3 bg-red-500 hover:bg-red-400'}><TrashIcon className='h-5 w-5'/>Slett medlem</Button>
+      </div>  
     </div>
   );
 
