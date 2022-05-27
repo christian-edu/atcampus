5270e41dbfc434e74a2c1a65a1480adf80022ce1
Christian Gregersen <christian@gregersen.eu>
2022-05-27 13:25:57 +0200

    Ser ut til å funke. Får se når vi tar en full purge senere

--- server/migration/1653646279878-initialData.ts
+++ server/migration/1653646279878-initialData.ts
@@ -4,21 +4,10 @@ import { v4 as uuidv4 } from "uuid";
 export class initialData1653646279878 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `INSERT INTO SCHOOL(name, uuid)
-             VALUES (HK, ${uuidv4()}),
-                    (BI, ${uuidv4()}),
-                    (OsloMet, ${uuidv4()}),
-                    (UiO, ${uuidv4()}),
-                    (UiS, ${uuidv4()}),
-                    (UiT, ${uuidv4()}),
-                    (UiB, ${uuidv4()}),
-                    (MIT, ${uuidv4()});
-            INSERT INTO USERS(username, email, password, school, uuid)
-            VALUES ('jimbob', 'jim@bob.com,' '$2b$10$io2NKvQX3TN.ULzd.FxhW.F0rQE57HUbcvl0muVYJgTem3uZzJAZ6',
-                    (SELECT uuid FROM SCHOOL WHERE NAME = 'MIT'), ${uuidv4()}),
-                   ('joebob', 'joe@bob.com', '$2b$10$io2NKvQX3TN.ULzd.FxhW.F0rQE57HUbcvl0muVYJgTem3uZzJAZ6',
-                    (SELECT uuid FROM SCHOOL WHERE NAME = 'MIT'), ${uuidv4()});
-            `
+      `INSERT INTO schools(name, uuid)VALUES ('HK', '${uuidv4()}'),('BI', '${uuidv4()}'),('OsloMet', '${uuidv4()}'),('UiO', '${uuidv4()}'),('UiS', '${uuidv4()}'),('UiT', '${uuidv4()}'),('UiB', '${uuidv4()}'),('MIT', '${uuidv4()}');`
+    );
+    await queryRunner.query(
+      `INSERT INTO users(username, email, password, school, uuid)VALUES ('jimbob', 'jim@bob.com,' '$2b$10$io2NKvQX3TN.ULzd.FxhW.F0rQE57HUbcvl0muVYJgTem3uZzJAZ6',(SELECT uuid FROM SCHOOL WHERE NAME = 'MIT'), '${uuidv4()}'),('joebob', 'joe@bob.com', '$2b$10$io2NKvQX3TN.ULzd.FxhW.F0rQE57HUbcvl0muVYJgTem3uZzJAZ6',(SELECT uuid FROM SCHOOL WHERE NAME = 'MIT'), '${uuidv4()}');`
     );
   }
 
