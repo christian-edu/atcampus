164f5028b6243f4b81afed8cf8bcfe9babaa7c10
Sigurd Kvamme <sigurdkvamme@live.no>
2022-05-28 01:39:16 +0200

    chat

---
+++ client/components/shared/ChatMessage.jsx
@@ -0,0 +1,31 @@
+import Image from './Image';
+
+const ChatMessage = ({ message }) => {
+  const transformDate = (date) => {
+    const messageDate = new Date(date);
+    return new Intl.DateTimeFormat('no-NB', {
+      weekday: 'long',
+      hour: '2-digit',
+      minute: '2-digit',
+    }).format(messageDate);
+  };
+
+  return (
+    <div className='pt-4 flex gap-4'>
+      <div className='h-12 w-12 flex-shrink-0'>
+        <Image />
+      </div>
+      <div className='flex flex-col gap-2'>
+        <div className='flex items-center gap-2'>
+          <h3 className='font-bold'>{message.userName}</h3>
+          <p className='text-dark-3 text-xs'>
+            {transformDate(message.timestamp)}
+          </p>
+        </div>
+        <p className='bg-dark-6 rounded-xl p-2 self-start'>{message.message}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ChatMessage;
