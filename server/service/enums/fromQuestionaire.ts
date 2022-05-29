d5d0e543086fdc39e129e558fe8657ffac8785e6
Generoman <53780885+Generoman@users.noreply.github.com>
2022-05-29 18:04:08 +0200

    updated search criteria weight values to mach questionnaire

--- server/service/enums/fromQuestionaire.ts
+++ server/service/enums/fromQuestionaire.ts
@@ -1,62 +1,62 @@
 export enum SchoolWeight {
-  FIVE = 39.5,
-  FOUR = 36.8,
-  THREE = 18.4,
-  TWO = 2.6,
-  ONE = 2.6,
+  FIVE = 41.4,
+  FOUR = 36.2,
+  THREE = 12.1,
+  TWO = 3.4,
+  ONE = 6.9,
   SUM = FIVE * 5 + FOUR * 4 + THREE * 3 + TWO * 2 + ONE,
 }
 
 export enum SubjectWeight {
-  FIVE = 15.8,
-  FOUR = 31.6,
-  THREE = 34.2,
-  TWO = 15.8,
-  ONE = 2.6,
+  FIVE = 15.5,
+  FOUR = 34.5,
+  THREE = 27.6,
+  TWO = 17.2,
+  ONE = 5.2,
   SUM = FIVE * 5 + FOUR * 4 + THREE * 3 + TWO * 2 + ONE,
 }
 
 export enum LocationWeight {
-  FIVE = 18.4,
-  FOUR = 34.2,
-  THREE = 28.9,
-  TWO = 10.5,
-  ONE = 7.9,
+  FIVE = 20.7,
+  FOUR = 37.9,
+  THREE = 22.4,
+  TWO = 8.6,
+  ONE = 10.3,
   SUM = FIVE * 5 + FOUR * 4 + THREE * 3 + TWO * 2 + ONE,
 }
 
 export enum GradeGoalWeight {
-  FIVE = 60.5,
-  FOUR = 18.4,
-  THREE = 10.5,
-  TWO = 5.3,
-  ONE = 5.3,
+  FIVE = 55.2,
+  FOUR = 12.1,
+  THREE = 17.2,
+  TWO = 6.9,
+  ONE = 8.6,
   SUM = FIVE * 5 + FOUR * 4 + THREE * 3 + TWO * 2 + ONE,
 }
 
 export enum LanguageWeight {
-  FIVE = 15.8,
-  FOUR = 34.2,
-  THREE = 18.4,
-  TWO = 10.5,
-  ONE = 21.1,
+  FIVE = 20.7,
+  FOUR = 31.0,
+  THREE = 13.8,
+  TWO = 15.5,
+  ONE = 17.2,
   SUM = FIVE * 5 + FOUR * 4 + THREE * 3 + TWO * 2 + ONE,
 }
 
 export enum WorkFrequencyWeight {
-  FIVE = 31.6,
-  FOUR = 50.0,
-  THREE = 15.8,
-  TWO = 2.6,
+  FIVE = 32.8,
+  FOUR = 43.1,
+  THREE = 22.4,
+  TWO = 1.7,
   ONE = 0.0,
   SUM = FIVE * 5 + FOUR * 4 + THREE * 3 + TWO * 2 + ONE,
 }
 
 export enum WorkTypeWeight {
-  FIVE = 44.7,
-  FOUR = 34.2,
-  THREE = 18.4,
-  TWO = 2.6,
+  FIVE = 44.8,
+  FOUR = 32.8,
+  THREE = 19.0,
+  TWO = 3.4,
   ONE = 0.0,
   SUM = FIVE * 5 + FOUR * 4 + THREE * 3 + TWO * 2 + ONE,
 }
