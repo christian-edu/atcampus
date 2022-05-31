90081460b4232111756e48f48c2b75fd1c8ea0a7
Sigurd Kvamme <sigurdkvamme@live.no>
2022-05-31 23:06:17 +0200

    updated group settings

--- client/components/GroupLinks.jsx
+++ client/components/GroupLinks.jsx
@@ -43,39 +43,35 @@ const GroupLinks = () => {
   return (
     <>
       <Breadcrumbs />
-<div>
-      <motion.div
-      initial={{ opacity: 0}}
-      animate={{ opacity: 1}}
-      >
-        <div className='grid gap-4 sm:grid-cols-2 lg:grid-cols-4 mb-8 mt-6'>
-          <Button to='/createGroup' className='lg:col-start-2'>
-            Opprett gruppe
-          </Button>
-          <Button to='/searchGroup' className='lg:col-start-3'>
-            Søk etter gruppe
-          </Button>
-        </div>
+      <div>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+          <div className='grid gap-4 sm:grid-cols-2 lg:grid-cols-4 mb-8 mt-6'>
+            <Button to='/createGroup' className='lg:col-start-2'>
+              Opprett gruppe
+            </Button>
+            <Button to='/searchGroup' className='lg:col-start-3'>
+              Søk etter gruppe
+            </Button>
+          </div>
         </motion.div>
         <motion.div
-        initial={{ opacity: 0}}
-        animate={{ opacity: 1}}
-        transition={{ delay: 0.2}}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.2 }}
         >
-        <h2 className='text-dark-1 text-xl font-bold mb-4'>Mine grupper</h2>
-        <div className='grid gap-x-4 sm:grid-cols-2 xl:grid-cols-3'>
-          {/*Only return this id groupdata is nbot undefined*/}
-          {groups.map((group) => (
-            <GroupCard
-              group={group}
-              key={group.uuid}
-              onClick={() => navigate(`/groups/${group.uuid}`)}
-            />
-          ))}
-        </div>
+          <h2 className='text-dark-1 text-xl font-bold mb-4'>Mine grupper</h2>
+          <div className='grid gap-x-4 sm:grid-cols-2 xl:grid-cols-3'>
+            {/*Only return this id groupdata is nbot undefined*/}
+            {groups.map((group) => (
+              <GroupCard
+                group={group}
+                key={group.uuid}
+                onClick={() => navigate(`/groups/${group.uuid}`)}
+              />
+            ))}
+          </div>
         </motion.div>
-        </div>
-      
+      </div>
     </>
   );
   // Return this only if they are logged in
