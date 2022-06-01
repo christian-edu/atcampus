a734f1d9a29350f75f52d5cd7673d83ebb369a4e
Emilio Garcia <emilio.garcia@mail.com>
2022-06-02 01:05:03 +0200

    match.com

--- client/components/shared/GroupCard.jsx
+++ client/components/shared/GroupCard.jsx
@@ -48,36 +48,38 @@ const GroupCard = ({ group, onClick, match, search, score }) => {
       </Modal>
 
       <div
-        className="bg-white flex items-center gap-2 relative p-6 my-3 rounded-standard border border-purple-4 cursor-pointer hover:drop-shadow-xl duration-300"
+        className="bg-white flex flex-col gap-4 p-6 my-3 rounded-standard border border-purple-4 cursor-pointer hover:drop-shadow-xl duration-300"
         onClick={clickHandler}
       >
-        <Image group className="h-14" />
-        <div>
-          <h3 className="text-dark-1 text-lg font-bold">{name}</h3>
-          <div className="flex flex-row items-center text-dark-3 mt-2">
-            <UserGroupIcon className="h-5 w-5 mr-2" />
+        {score ? 
+              <motion.div
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              transition={{ type: "spring", duration: 0.8, delay: 0.4}}
+              className="bg-gradient-left text-sm text-white px-2 py-1 text-center rounded-full w-24 whitespace-nowrap -mt-10 mx-auto drop-shadow-[0_3px_15px_rgba(127,124,202,0.50)]">
+              <p><span className="font-bold">{score}%</span> match</p> 
+              </motion.div>
+              : <></>}
+        <div className="flex items-center gap-2">
+          <Image group className="h-14" />
+          <div>
+            <h3 className="text-dark-1 text-lg font-bold">{name}</h3>
+            <div className="flex flex-row items-center text-dark-3 mt-2">
+              <UserGroupIcon className="h-5 w-5 mr-2" />
 
-            <p className="text-md">Medlemmer: {groupMembers.length}</p>
+              <p className="text-md">Medlemmer: {groupMembers.length}</p>
+            </div>
+
+            
           </div>
-          
-            {score ? 
-            <motion.div
-            initial={{ scale: 0 }}
-            animate={{ scale: 1 }}
-            transition={{ type: "spring", duration: 0.8, delay: 0.4}}
-            className="bg-purple-1 text-sm text-white px-2 py-1 text-center rounded-full w-24 whitespace-nowrap mt-3 -mb-10 mx-auto drop-shadow-[0_5px_15px_rgba(127,124,202,0.75)]">
-            <p><span className="font-bold">{score}%</span> match</p> 
-            </motion.div>
-            : <></>}
-          
-          
-        </div>
-        {/* Render plus icon if search or match is true */}
-        {(search || match) && (
-          <PlusIcon className="h-6 text-purple-1 ml-auto" />
-        )}
+          {/* Render plus icon if search or match is true */}
+          {(search || match) && (
+            <PlusIcon className="h-6 text-purple-1 ml-auto" />
+          )}
 
-        {!(search || match) && privateIcon}
+          {!(search || match) && privateIcon}
+        </div>
+        
       </div>
     </>
   );
